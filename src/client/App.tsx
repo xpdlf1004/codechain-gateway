1a53f118982fc58b8785c32bcf64ac93113971eb
JinGyeong Jeong <jjg@kodebox.io>
2018-10-31 15:27:07 +0900

    Add Account page

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,19 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
+import { Account } from "./pages/Account";
 import { Home } from "./pages/Home";
 
 export const App = () => (
   <Router>
     <div>
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/account">Account</Link>
+      <hr />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/account" component={Account} />
         <Route component={NotFound} />
       </Switch>
     </div>
