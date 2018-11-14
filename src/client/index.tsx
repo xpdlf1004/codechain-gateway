6ab56519c4d9f5afbf7e9ae61b305e738cb48ecc
Gilyoung <roadzero@kodebox.io>
2018-11-14 16:18:27 +0900

    Import Bootstrap and Reactstrap

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import { App } from "./App";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
